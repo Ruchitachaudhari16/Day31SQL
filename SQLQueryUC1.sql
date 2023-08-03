47d99c4e19195efb0d3955c072078e4f85873c8b
Ruchitachaudhari16 <ruchitachaudhari70@gmail.com>
2023-08-03 14:07:43 -0400

    [Ruchita]Add:Create AddressBook Service DB

---
+++ SQLQueryUC1.sql
@@ -0,0 +1,29 @@
+--Abcc
+--SQL stand for structured Query Language 
+--it is used to communicate with the database
+--it is used to CURD operations (create,update,retrieve,delete)
+--SQL commands are instructions used to perform a specific tasks.they are
+--DDL-Data Definition Language
+     --Create
+	 --ALTER
+	 --DROP
+	 --TRUNCATE
+--DML-Data Manipulation Language
+      --INSERT
+	  --UPDATE
+	  --DELETE
+--DCL-Data Control Language
+      --Revoke
+	  --Grant
+--DQL-Data Query Language
+     --SELECT
+--TCL-Transaction Control Language
+     --Commit
+	 --Rollback
+	 --Savepoint
+
+	 --UC1:- Create Database
+		CREATE DATABASE AddressBookService
+		USE AddressBookService;
+
+		
